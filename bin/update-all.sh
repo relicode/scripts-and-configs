29c9493c5df3397101683be611fd6e66bb05f1b4
Anssi Siren <pro@avaruus.mozmail.com>
2023-12-19 11:26:59 +0100

    Add executables

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-for cmd in update dist-upgrade autoremove autoclean
-do
-  apt -y "${cmd}"
-done
+IS_KDE="$(lsb_release -a 2>/dev/null | grep 'KDE neon' 1>/dev/null && printf true)"
+
+if [ "$IS_KDE" ]; then
+  for CMD in refresh update autoremove; do
+    sudo pkcon -y "$CMD"
+  done
+else
+  for CMD in update dist-upgrade autoremove autoclean; do
+    sudo apt -y "$CMD"
+  done
+fi
