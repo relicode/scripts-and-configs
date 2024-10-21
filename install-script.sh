a4c8735bea6970ebee9943efaa9f1167041ed71d
Anssi Siren <pro@avaruus.mozmail.com>
2024-10-21 12:01:40 +0300

    Add bash-aliases

--- install-script.sh
+++ install-script.sh
@@ -70,45 +70,10 @@ install_nvchad () {
 update_submodules () {
   git submodule foreach "\
     echo Submodule $sm_path at commit "$(git rev-parse HEAD)"; \
-    echo Rebasing to main...; \
+    echo Rebasing to master...; \
     git checkout master; \
     git pull origin master; \
     echo DONE\!; \
     echo"
 }
 
-display_help () {
-  echo
-  if [ -z "$2" ]; then
-    echo "Invalid command: '$1' - valid commands are:"
-    echo init_dirs
-    echo install_deps
-    echo install_extras
-    echo install_docker
-    echo install_ohmytmux
-    echo install_nvim
-    echo compile_nvim
-    echo install_nvm
-    echo install_python_venv
-    echo install_nvchad
-    echo update_submodules
-  else
-    echo "$2"
-  fi
-  exit 1
-}
-
-if [ "$#" -lt 1 ]; then
-  display_help help
-fi
-
-for COMMAND in $@; do
-  case "$COMMAND" in
-    init_dirs|install_deps|install_docker|install_ohmytmux|install_nvim|compile_nvim|install_extras|install_nvm|install_python_venv|install_nvchad|update_submodules)
-      $COMMAND || display_help "$COMMAND" "Failed running command "$COMMAND""
-      ;;
-    *)
-      display_help "$COMMAND"
-      ;;
-  esac
-done
